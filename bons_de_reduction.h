3c8015690fe7a20ebbbaaf556fcae8ae1ffaaad4
rihemomrani <rihem.omrani@esprit.tn>
2021-12-06 14:20:00 +0100

    commit from gestion_offres sur bons_de_reduction.h

---
+++ bons_de_reduction.h
@@ -0,0 +1,13 @@
+#ifndef BONS_DE_REDUCTION_H
+#define BONS_DE_REDUCTION_H
+#include <qsqlquerymodel.h>
+
+class bons_de_reduction
+{
+public:
+    bons_de_reduction();
+    QSqlQueryModel * trier_nb();
+
+};
+
+#endif // BONS_DE_REDUCTION_H
