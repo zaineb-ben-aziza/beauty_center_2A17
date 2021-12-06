b7dbac59a43d6a7238fc13c3ec06c01611184abb
rihemomrani <rihem.omrani@esprit.tn>
2021-12-06 14:16:31 +0100

    commit from gestion_offres sur arduino.h

---
+++ arduino.h
@@ -0,0 +1,31 @@
+#ifndef ARDUINO_H
+#define ARDUINO_H
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+#include <QDebug>
+
+
+class Arduino
+{
+public:     //méthodes de la classe Arduino
+    Arduino();
+    int connect_arduino(); // permet de connecter le PC à Arduino
+    int close_arduino(); // permet de femer la connexion
+    int write_to_arduino( QByteArray ); // envoyer des données vers arduino
+
+    QSerialPort* getserial();   // accesseur
+    QString getarduino_port_name();
+private:
+    QByteArray data;
+QSerialPort * serial; //Cet objet rassemble des informations (vitesse, bits de données, etc.)
+//et des fonctions (envoi, lecture de réception,…) sur ce qu’est une voie série pour Arduino.
+static const quint16 arduino_uno_vendor_id=9025;
+static const quint16 arduino_uno_producy_id=67;
+QString arduino_port_name;
+bool arduino_is_available;
+
+};
+
+
+
+#endif // ARDUINO_H
